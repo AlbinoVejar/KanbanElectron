2bfaa7fb58e52b212ecddce19aa6681121f79d3a
Albino Vejar <albinovejar@hotmail.com>
2020-08-28 19:07:48 -0700

    Actualización para la consulta en la base de datos.

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -1,9 +1,20 @@
+import { async } from '@angular/core/testing';
 import { Injectable } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
+import { HttpClient } from '@angular/common/http';
+// import * as sqlite3 from 'sqlite3';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
-
-  constructor() { }
+  constructor(
+    private electronS: ElectronService,
+    private http: HttpClient,
+  ) {
+    this.electronS.ipcRenderer.send('hola');
+    this.electronS.ipcRenderer.on('adios', (event, args) => {
+      console.log(args);
+    });
+  }
 }
